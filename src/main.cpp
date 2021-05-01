7e958571bd41f40546f86ff83b2de05de2421eb8
Davinais <david870620@gmail.com>
2021-05-02 02:53:41 +0800

    Add option to plot

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@ int main(int argc, char** argv)
 {
     fstream input_blk, input_net, output;
     double alpha;
+    bool plot = false;
+    string plot_file = "best.png";
 
-    if (argc == 5) {
+    if (argc >= 5 && argc <= 7) {
         alpha = stod(argv[1]);
         input_blk.open(argv[2], ios::in);
         input_net.open(argv[3], ios::in);
@@ -30,10 +32,18 @@ int main(int argc, char** argv)
                  << "\". The program will be terminated..." << endl;
             exit(1);
         }
+        if (argc >= 6) {
+            if (argv[5] == string("-p")) {
+                plot = true;
+                if (argc == 7) {
+                    plot_file = argv[6];
+                }
+            }
+        }
     }
     else {
         cerr << "Usage: ./Floorplanner <alpha> <input block file> " <<
-                "<input net file> <output file>" << endl;
+                "<input net file> <output file> <-p(optional)> <plot file(optional)>" << endl;
         exit(1);
     }
 
@@ -43,7 +53,9 @@ int main(int argc, char** argv)
     auto stop = chrono::high_resolution_clock::now();
     chrono::duration<double> exe_time = stop - start;
     fp.printSummary(output, exe_time.count());
-    fp.plot("best.png");
+    if (plot) {
+        fp.plot(plot_file);
+    }
 
     return 0;
 }
